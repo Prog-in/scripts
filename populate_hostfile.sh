50cefe853fd35f7479abf3fe3e05e7368fc439e2
Prog-in <joaogabrielferes@gmail.com>
2023-11-30 14:47:28 -0300

    .

--- populate_hostfile.sh
+++ populate_hostfile.sh
@@ -5,8 +5,8 @@ HOSTFILE_PATH="$NFS_DIR/hostfile.txt"
 INSTANCE_IP=$(ip addr show | grep -Eo 'inet (([0-9]{1,3}\.){3}[0-9]{1,3})' | grep -vE '127\.0\.0\.1|172\.[1-9]\.|10\.[0-9]\.|192\.168\.' | awk '{print $2}')
 
 # Verifica se o arquivo existe localmente, se não, cria
-if [ ! -f "$NFS_DIR/hostfile_local.txt" ]; then
-    echo "$INSTANCE_IP" > "$NFS_DIR/hostfile_local.txt"
+if [ ! -f "$HOSTFILE_PATH" ]; then
+    echo "$INSTANCE_IP" > "$HOSTFILE_PATH"
 fi
 
 # Acrescenta o endereço IP ao arquivo no compartilhamento NFS
